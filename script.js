0ebdddbb675f9e791587faf2e2a39c5dd81cf1bf
kevin <kevin_chaney@icloud.com>
2022-10-03 00:26:57 -0700

    Added userInput for selection

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 //PSEUDOCODE
 
 //user INPUTS either rock, paper, or scissors
+function getUserChoice() {
+    let userChoice = prompt('Please select rock, paper, or scissors')
+    console.log(userChoice);
+    if (userChoice == null) {
+        alert('No input was made')
+    }
+}
+getUserChoice()
 // computer randomly selects rock, paper, or scissors
 function getComputerChoice() {
     let choices = ['rock', 'paper', 'scissors'];
     let choice = choices[Math.floor(Math.random()*choices.length)];
     return choice
 }
-let a = 5;
 // IF user AND computer select the same choice 
 //     RETURN message saying 'it is a tie'
 // IF user selects rock AND computer selects paper OR 
